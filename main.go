78426e51c233ff72f202a1a03207546c1f6630d9
Hohol <djuvotnoe@gmail.com>
2025-06-12 23:52:54 +0200

    added Reader interface

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"strings"
 )
 
 func main() {
@@ -13,4 +15,16 @@ func main() {
 
 	fmt.Println(*city)
 	fmt.Println(*format)
+
+	r := strings.NewReader("Hello, World!")
+	block := make([]byte, 4)
+
+	for {
+		_, err := r.Read(block)
+		fmt.Printf("%q\n", block)
+		if err == io.EOF {
+			break
+		}
+	}
+
 }
