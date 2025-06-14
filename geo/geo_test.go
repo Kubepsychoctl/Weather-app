78f28f17add78e703bc24c771e68ca3c9d96ce05
Hohol <djuvotnoe@gmail.com>
2025-06-14 23:49:56 +0200

    provided simple unit-integration test

---
+++ geo/geo_test.go
@@ -0,0 +1,23 @@
+package geo_test
+
+import (
+	"testing"
+	"weather-app/geo"
+)
+
+func TestGetMyLocation(t *testing.T) {
+	// Arrange
+	city := "Bishkek"
+	expected := geo.GeoData{
+		City: "Bishkek",
+	}
+
+	// Act
+	got, err := geo.GetMyLocation(city)
+	if err != nil {
+		t.Error("Error getting city", err)
+	}
+	if got.City != expected.City {
+		t.Errorf("Expected city %v, got %v", expected.City, got.City)
+	}
+}
