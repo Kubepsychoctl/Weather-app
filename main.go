604988af5a5785adda7df07ffb8561dd8520cfae
Hohol <djuvotnoe@gmail.com>
2025-06-14 13:10:56 +0200

    injected geo usage

--- main.go
+++ main.go
@@ -3,28 +3,19 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
-	"strings"
+
+	"weather-app/geo"
 )
 
 func main() {
 	city := flag.String("city", "", "City of the user")
-	format := flag.Int("format", 1, "Format of the weather")
 
 	flag.Parse()
 
 	fmt.Println(*city)
-	fmt.Println(*format)
-
-	r := strings.NewReader("Hello, World!")
-	block := make([]byte, 4)
-
-	for {
-		_, err := r.Read(block)
-		fmt.Printf("%q\n", block)
-		if err == io.EOF {
-			break
-		}
+	geoData, err := geo.GetMyLocation(*city)
+	if err != nil {
+		fmt.Println("Error getting my location:", err)
 	}
-
+	fmt.Println(geoData)
 }
